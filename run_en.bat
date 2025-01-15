186051a164f9b865a8f12e28248d39a5db1d7aea
aoguai <1340219674@qq.com>
2025-01-15 14:56:34 +0800

    chore: update bat

--- run_en.bat
+++ run_en.bat
@@ -25,6 +25,35 @@ echo LICENSE AGPL-3.0 license
 echo ----------------------------------------
 
 REM Prompt user for input parameters
+set /p "layout_only=Layout only without changing background (yes/no, default is no): "
+if /i "!layout_only!"=="yes" (
+    set layout_only=--layout-only
+    set change_background=--no-change-background
+    set save_background=--no-save-background
+    set rgb_list=255,255,255
+) else (
+    set layout_only=
+    set /p "change_background=Change background (yes/no, default is no): "
+    if /i "!change_background!"=="yes" (
+        set change_background=--change-background
+        set /p "rgb_list=Enter RGB channel values (comma separated, default is 255,255,255): "
+        if "!rgb_list!"=="red" set rgb_list=255,0,0
+        if "!rgb_list!"=="blue" set rgb_list=12,92,165
+        if "!rgb_list!"=="white" set rgb_list=255,255,255
+        if "!rgb_list!"=="" set rgb_list=255,255,255
+        set /p "save_background=Save images with changed background (yes/no, default is no): "
+        if /i "!save_background!"=="yes" (
+            set save_background=--save-background
+        ) else (
+            set save_background=--no-save-background
+        )
+    ) else (
+        set change_background=--no-change-background
+        set save_background=--no-save-background
+        set rgb_list=255,255,255
+    )
+)
+
 set /p "resize=Resize images (yes/no, default is yes): "
 if /i "!resize!"=="no" (
     set resize=--no-resize
@@ -58,26 +87,6 @@ if /i "!save_corrected!"=="yes" (
     set save_corrected=--no-save-corrected
 )
 
-set /p "change_background=Change background (yes/no, default is no): "
-if /i "!change_background!"=="yes" (
-    set change_background=--change-background
-    set /p "rgb_list=Enter RGB channel values (comma separated, default is 255,255,255): "
-    if "!rgb_list!"=="red" set rgb_list=255,0,0
-    if "!rgb_list!"=="blue" set rgb_list=12,92,165
-    if "!rgb_list!"=="white" set rgb_list=255,255,255
-    if "!rgb_list!"=="" set rgb_list=255,255,255
-    set /p "save_background=Save images with changed background (yes/no, default is no): "
-    if /i "!save_background!"=="yes" (
-        set save_background=--save-background
-    ) else (
-        set save_background=--no-save-background
-    )
-) else (
-    set change_background=--no-change-background
-    set save_background=--no-save-background
-    set rgb_list=255,255,255
-)
-
 set /p "sheet_rows=Enter the number of rows in the photo sheet (default is 3): "
 if "!sheet_rows!"=="" set sheet_rows=3
 
@@ -100,7 +109,7 @@ if exist "%INPUT_PATH%\" (
         set "OUTPUT_PATH=%%~dpnf_output%%~xf"
         
         REM Execute Python script to process the image
-        start "" cmd /k "%PYTHON_EXE% %SCRIPT_PATH% "%%~ff" -b !rgb_list! -s "%%~dpnf_output%%~xf" -p !photo_type! --photo-sheet-size !photo_sheet_size! !compress! !save_corrected! !change_background! !save_background! -sr !sheet_rows! -sc !sheet_cols! !rotate! !resize! !save_resized! & pause"
+        start "" cmd /k "%PYTHON_EXE% %SCRIPT_PATH% "%%~ff" -b !rgb_list! -s "%%~dpnf_output%%~xf" -p !photo_type! --photo-sheet-size !photo_sheet_size! !compress! !save_corrected! !change_background! !save_background! -sr !sheet_rows! -sc !sheet_cols! !rotate! !resize! !save_resized! !layout_only! & pause"
     )
 ) else (
     REM If it's a file, process the file directly
@@ -109,7 +118,7 @@ if exist "%INPUT_PATH%\" (
     set OUTPUT_PATH=%INPUT_DIR%%~n1_output%~x1
     
     REM Due to setlocal enabledelayedexpansion, use !variable_name! to reference variables
-    start "" cmd /k "%PYTHON_EXE% %SCRIPT_PATH% "!INPUT_PATH!" -b !rgb_list! -s "!OUTPUT_PATH!" -p !photo_type! --photo-sheet-size !photo_sheet_size! !compress! !save_corrected! !change_background! !save_background! -sr !sheet_rows! -sc !sheet_cols! !rotate! !resize! !save_resized! & pause"
+    start "" cmd /k "%PYTHON_EXE% %SCRIPT_PATH% "!INPUT_PATH!" -b !rgb_list! -s "!OUTPUT_PATH!" -p !photo_type! --photo-sheet-size !photo_sheet_size! !compress! !save_corrected! !change_background! !save_background! -sr !sheet_rows! -sc !sheet_cols! !rotate! !resize! !save_resized! !layout_only! & pause"
 )
 
 pause
--- run_zh.bat
+++ run_zh.bat
@@ -2,7 +2,7 @@
 set CLI_LANGUAGE=zh
 setlocal enabledelayedexpansion
 
-REM 获取当前批处理文件目录
+REM 获取当前批处理文件的目录
 set SCRIPT_DIR=%~dp0
 
 REM 设置Python解释器路径和项目目录
@@ -21,70 +21,79 @@ set INPUT_PATH=%~1
 
 echo                  LiYing
 echo Github: https://github.com/aoguai/LiYing
-echo LICENSE AGPL-3.0 license
+echo 许可证: AGPL-3.0
 echo ----------------------------------------
 
 REM 提示用户输入参数
-set /p "resize=是否调整图像尺寸（是/否，默认是）："
+set /p "layout_only=是否仅进行布局而不更改背景（是/否，默认为否）: "
+if /i "!layout_only!"=="是" (
+    set layout_only=--layout-only
+    set change_background=--no-change-background
+    set save_background=--no-save-background
+    set rgb_list=255,255,255
+) else (
+    set layout_only=
+    set /p "change_background=是否更改背景（是/否，默认为否）: "
+    if /i "!change_background!"=="是" (
+        set change_background=--change-background
+        set /p "rgb_list=输入RGB通道值（用逗号分隔，默认为255,255,255）: "
+        if "!rgb_list!"=="红" set rgb_list=255,0,0
+        if "!rgb_list!"=="蓝" set rgb_list=12,92,165
+        if "!rgb_list!"=="白" set rgb_list=255,255,255
+        if "!rgb_list!"=="" set rgb_list=255,255,255
+        set /p "save_background=是否保存更改背景后的图像（是/否，默认为否）: "
+        if /i "!save_background!"=="是" (
+            set save_background=--save-background
+        ) else (
+            set save_background=--no-save-background
+        )
+    ) else (
+        set change_background=--no-change-background
+        set save_background=--no-save-background
+        set rgb_list=255,255,255
+    )
+)
+
+set /p "resize=是否调整图像大小（是/否，默认为是）: "
 if /i "!resize!"=="否" (
     set resize=--no-resize
     set save_resized=--no-save-resized
 ) else (
     set resize=--resize
-    set /p "save_resized=是否保存调整尺寸后的图像（是/否，默认否）："
+    set /p "save_resized=是否保存调整大小后的图像（是/否，默认为否）: "
     if /i "!save_resized!"=="是" (
         set save_resized=--save-resized
     ) else (
         set save_resized=--no-save-resized
     )
-    set /p "photo_type=输入照片类型（默认为一寸）："
+    set /p "photo_type=输入照片类型（默认为一寸）: "
     if "!photo_type!"=="" set photo_type=一寸
 )
 
-set /p "photo_sheet_size=输入照片表格尺寸（默认为五寸）："
+set /p "photo_sheet_size=输入照片表格大小（默认为五寸）: "
 if "!photo_sheet_size!"=="" set photo_sheet_size=五寸
 
-set /p "compress=是否压缩图像（是/否，默认否）："
+set /p "compress=是否压缩图像（是/否，默认为否）: "
 if /i "!compress!"=="是" (
     set compress=--compress
 ) else (
     set compress=--no-compress
 )
 
-set /p "save_corrected=是否保存修正后的图像（是/否，默认否）："
+set /p "save_corrected=是否保存校正后的图像（是/否，默认为否）: "
 if /i "!save_corrected!"=="是" (
     set save_corrected=--save-corrected
 ) else (
     set save_corrected=--no-save-corrected
 )
 
-set /p "change_background=是否更换背景（是/否，默认否）："
-if /i "!change_background!"=="是" (
-    set change_background=--change-background
-    set /p "rgb_list=输入RGB通道值（逗号分隔，默认为255,255,255）："
-    if "!rgb_list!"=="红色" set rgb_list=255,0,0
-    if "!rgb_list!"=="蓝色" set rgb_list=12,92,165
-    if "!rgb_list!"=="白色" set rgb_list=255,255,255
-    if "!rgb_list!"=="" set rgb_list=255,255,255
-    set /p "save_background=是否保存更换背景后的图像（是/否，默认否）："
-    if /i "!save_background!"=="是" (
-        set save_background=--save-background
-    ) else (
-        set save_background=--no-save-background
-    )
-) else (
-    set change_background=--no-change-background
-    set save_background=--no-save-background
-    set rgb_list=255,255,255
-)
-
-set /p "sheet_rows=输入照片表格的行数（默认为3）："
+set /p "sheet_rows=输入照片表格的行数（默认为3）: "
 if "!sheet_rows!"=="" set sheet_rows=3
 
-set /p "sheet_cols=输入照片表格的列数（默认为3）："
+set /p "sheet_cols=输入照片表格的列数（默认为3）: "
 if "!sheet_cols!"=="" set sheet_cols=3
 
-set /p "rotate=是否旋转照片90度（是/否，默认否）："
+set /p "rotate=是否将照片旋转90度（是/否，默认为否）: "
 if /i "!rotate!"=="是" (
     set rotate=--rotate
 ) else (
@@ -99,8 +108,8 @@ if exist "%INPUT_PATH%\" (
         set "INPUT_FILE=%%~ff"
         set "OUTPUT_PATH=%%~dpnf_output%%~xf"
         
-        REM 执行Python脚本来处理图像
-        start "" cmd /k "%PYTHON_EXE% %SCRIPT_PATH% "%%~ff" -b !rgb_list! -s "%%~dpnf_output%%~xf" -p !photo_type! --photo-sheet-size !photo_sheet_size! !compress! !save_corrected! !change_background! !save_background! -sr !sheet_rows! -sc !sheet_cols! !rotate! !resize! !save_resized! & pause"
+        REM 执行Python脚本处理图像
+        start "" cmd /k "%PYTHON_EXE% %SCRIPT_PATH% "%%~ff" -b !rgb_list! -s "%%~dpnf_output%%~xf" -p !photo_type! --photo-sheet-size !photo_sheet_size! !compress! !save_corrected! !change_background! !save_background! -sr !sheet_rows! -sc !sheet_cols! !rotate! !resize! !save_resized! !layout_only! & pause"
     )
 ) else (
     REM 如果是文件，直接处理该文件
@@ -109,7 +118,7 @@ if exist "%INPUT_PATH%\" (
     set OUTPUT_PATH=%INPUT_DIR%%~n1_output%~x1
     
     REM 由于使用了setlocal enabledelayedexpansion，使用!variable_name!来引用变量
-    start "" cmd /k "%PYTHON_EXE% %SCRIPT_PATH% "!INPUT_PATH!" -b !rgb_list! -s "!OUTPUT_PATH!" -p !photo_type! --photo-sheet-size !photo_sheet_size! !compress! !save_corrected! !change_background! !save_background! -sr !sheet_rows! -sc !sheet_cols! !rotate! !resize! !save_resized! & pause"
+    start "" cmd /k "%PYTHON_EXE% %SCRIPT_PATH% "!INPUT_PATH!" -b !rgb_list! -s "!OUTPUT_PATH!" -p !photo_type! --photo-sheet-size !photo_sheet_size! !compress! !save_corrected! !change_background! !save_background! -sr !sheet_rows! -sc !sheet_cols! !rotate! !resize! !save_resized! !layout_only! & pause"
 )
 
 pause
